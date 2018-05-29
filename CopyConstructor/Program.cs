aa294f30173d6f0defc44a290ad3c3bd56908722
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-05-28 21:08:05 -0700

    create CopyConstructor

---
+++ CopyConstructor/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CopyConstructor
+{
+    public class Person
+    {
+        public string[] Names;
+        public Address Address;
+        public Person(string[] names, Address address)
+        {
+            if (names == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(names));
+            }
+            if (address == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(address));
+            }
+            Names = names;
+            Address = address;;
+        }
+
+        public Person(Person other)
+        {
+            Names = other.Names;
+            Address = new Address(other.Address);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+
+        private object namoef(string[] names)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Address
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            if (streetName == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(streetName));
+            }
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public Address(Address other)
+        {
+            StreetName = other.StreetName;
+            HouseNumber = other.HouseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person( new[]{ "John", "Smith"}, new Address("London Road", 123));
+            var jane = new Person(john);
+
+            jane.Address.HouseNumber = 321;
+
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
+        }
+    }
+}
