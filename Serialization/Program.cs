7a39daf38208347956c0c485bc7ee5f72e86aeea
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-05-28 21:40:30 -0700

    create DeepCopy extensionMethods using Binary

---
+++ Serialization/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    public static class ExtensionMethods
+    {
+        // binary serializer
+        public static T DeepCopy<T>(this T self)
+        {
+            var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, self);
+            stream.Seek(0, SeekOrigin.Begin);
+            object copy = formatter.Deserialize(stream);
+            stream.Close();
+            return (T) copy;
+        }
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public string[] Names;
+        public Address Address;
+        public Person(string[] names, Address address)
+        {
+            if (names == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(names));
+            }
+            if (address == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(address));
+            }
+            Names = names;
+            Address = address;;
+        }
+
+        public Person(Person other)
+        {
+            Names = other.Names;
+            Address = new Address(other.Address);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+    }
+
+
+    [Serializable]
+    public class Address
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            if (streetName == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(streetName));
+            }
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public Address(Address other)
+        {
+            StreetName = other.StreetName;
+            HouseNumber = other.HouseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person( new[]{ "John", "Smith"}, new Address("London Road", 123));
+            var jane = john.DeepCopy();
+            jane.Names[0] = "Jane";
+            jane.Address.HouseNumber = 321;
+
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
+        }
+    }}
